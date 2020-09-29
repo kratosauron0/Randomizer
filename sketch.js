ebd1f2d4504373bc01505916246edddeb83c00f8
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-09-28 23:56:11 -0700

    Created Array
    Made canvas show names and colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+
+let lolChampions = [{
+
+  name: "Ashe",
+  ability: "Arrow"
+},{
+  name: "Garen",
+  ability: "Sword"
+
+},{
+  name: "Teemo",
+  ability: "Poison"
+
+},{
+  name: "Volibear",
+  ability: "Lightning"
+
+},{
+  name: "Annie",
+  ability: "Tibers"
+
+}];
+
+let randomIndex;
+
+
 function setup() {
   createCanvas(400, 400);
+
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed(){
+  background(random (200,255),0,0);
+  randomIndex= int(random(lolChampions.length));
+  text(lolChampions[randomIndex].name, 50,50);
+  lolChampions.splice(randomIndex, 1);
+
 }
