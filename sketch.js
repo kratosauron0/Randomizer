e15d76a72fc96dbea646f772eb28f08da1450214
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-09-30 23:52:16 -0700

    Randomizer animation
    added random abilities to the Canvas

--- sketch.js
+++ sketch.js
@@ -22,22 +22,46 @@ let lolChampions = [{
 }];
 
 let randomIndex;
-
+let animating = false;
 
 function setup() {
   createCanvas(400, 400);
+  textSize(32);
+  text ("Click to Randomize", 50,50);
+
 
 
 }
 
 function draw() {
 
+  if(animating==true){
+    ellipse(random(width), random(height), random(50,100));
+    }
+
 }
 
-function mousePressed(){
+function randomizer(){
+  animating = false;
   background(random (200,255),0,0);
-  randomIndex= int(random(lolChampions.length));
-  text(lolChampions[randomIndex].name, 50,50);
-  lolChampions.splice(randomIndex, 1);
+  if (lolChampions.length >0){
+
+    randomIndex= int(random(lolChampions.length));
+    text(`${lolChampions[randomIndex].name} 's ability
+      ${lolChampions[randomIndex].ability}`,50,50);
+
+    lolChampions.splice(randomIndex, 1);
+  }
+  else {
+    text("Nothing Left!", 50, 50)
+  }
+}
+
+
+
+function mousePressed(){
+  animating = true;
+  setTimeout(randomizer,2000);
+
 
 }
